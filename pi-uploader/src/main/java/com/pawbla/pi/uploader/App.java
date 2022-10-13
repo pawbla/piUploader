f84528bd5bde74db2a8b341dbf413907872e92a9
Pawel <blachut.pawel@gmail.com>
2022-10-13 18:55:59 +0200

    Initial commit

---
+++ pi-uploader/src/main/java/com/pawbla/pi/uploader/App.java
@@ -0,0 +1,13 @@
+package com.pawbla.pi.uploader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
